9ce2b3ea2a378df930d8efd9435c228544142543
danielaroldanb <89650193+danielaroldanb@users.noreply.github.com>
2022-01-28 15:12:04 -0500

    Cards Component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import Card from "./Card";
 
-export default function Cards(props) {
-  // acá va tu código
-  // tip, podés usar un map
-  return <div>Cards Component</div>
-};
+export default function Cards({ cities }) {
+  return (
+    <div>
+      {cities.map((city) => (
+        <Card
+          key={city.id}
+          min={city.main.temp_min}
+          max={city.main.temp_max}
+          name={city.name}
+          img={city.weather[0].icon}
+          onClose={() => alert(city.name)}
+        />
+      ))}
+    </div>
+  );
+}
