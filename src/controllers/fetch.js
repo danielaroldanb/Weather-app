76b030be2111e00b0335675a1b62c5e40975c436
danielaroldanb <89650193+danielaroldanb@users.noreply.github.com>
2022-02-01 15:40:44 -0500

    fetch

--- src/controllers/fetch.js
+++ src/controllers/fetch.js
@@ -2,7 +2,7 @@ const apiKey = process.env.REACT_APP_APIKEY;
 
 export default function fetchCity(city,setData){
     fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
       )
         .then((r) => r.json())
         .then((response) => {
